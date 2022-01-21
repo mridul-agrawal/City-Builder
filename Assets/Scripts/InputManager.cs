8b7f0f212382932959a06945cb7564c79898fc4a
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2022-01-21 16:58:09 +0530

    Implemented Getting Mouse Imput.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+    public LayerMask mouseInputLayer;
+
+
+    private void Update()
+    {
+        GetInput();
+    }
+
+    public void GetInput()
+    {
+        if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputLayer))
+            {
+                Vector3 position = hit.point - transform.position;
+                Debug.Log(position);
+            }
+        }
+    }
+}
