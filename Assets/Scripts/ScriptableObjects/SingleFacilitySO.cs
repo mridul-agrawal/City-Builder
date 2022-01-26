01dcd38f0c9cba5272bd3efc1f81f9d5fe6a8a35
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2022-01-26 10:29:20 +0530

    Created Data Layer with ScriptableObjects for all the structures to be used in the game.

---
+++ Assets/Scripts/ScriptableObjects/SingleFacilitySO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New facility", menuName = "CityBuilder/StructureData/Facility")]
+public class SingleFacilitySO : SingleStructureBaseSO
+{
+    public int maxCustomers;
+    public int upkeepPerCustomer;
+}
