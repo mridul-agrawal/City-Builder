785721fb6b512c67afdbdc3cf9c22d587bb0db06
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2022-02-02 16:32:33 +0530

    Implemented Structure Economy into the game (For Creation)

--- Assets/Scripts/BuildingManagerHelpers/StructureModificationHelpers.cs
+++ Assets/Scripts/BuildingManagerHelpers/StructureModificationHelpers.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -33,9 +34,11 @@ public abstract class StructureModificationHelper
     public virtual void ConfirmModifications()
     {
         placementManager.PlaceStructuresOnTheMap(structuresToBeModified.Values);
+        Type structureType = structureData.GetType();
         foreach (var keyValuePair in structuresToBeModified)
         {
             grid.PlaceStructureOnTheGrid(keyValuePair.Value, keyValuePair.Key, GameObject.Instantiate(structureData));
+            StructureEconomyManager.CreateStructureLogic(structureType, keyValuePair.Key, grid);
         }
         ResetHelpersData();
     }
